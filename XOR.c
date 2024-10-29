ec28704b087fa0fb4567e7e7987f2a4ecf56631c
ad <adrien.malgouyres@epita.fr>
2024-10-29 14:29:54 +0100

    [UPDATE] XOR.C is now an OOP program

--- XOR.c
+++ XOR.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <err.h>
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
 
-#define INPUT_NEURONS 2
-#define HIDDEN_NEURONS 2
-#define LAYERS 2
-#define LEARNING_RATE 0.0001
-#define EPOCHS 10000
-int NEURONS_NUMBER[LAYERS] = {2, 2};
-
+#define LAYER_NUMBER 3
+#define LEARNING_RATE 0.001
+#define EPOCHS 100000
 
 typedef struct neuron_t
 {
@@ -34,6 +32,11 @@ typedef struct layer_t
 
 
 
+layer* LAYERS;
+int NEURONS_NUMBER[LAYER_NUMBER] = {2, 2, 1};
+int** DESIRED_OUTPUTS;
+
+
 
 layer create_layer(int neuron_number) {
   neuron * neurons = calloc(neuron_number, sizeof(neuron));
@@ -44,251 +47,314 @@ layer create_layer(int neuron_number) {
 }
 
 neuron create_neuron(int out_connections) {
-  neuron neu;
-  neu.actv = 0.0f;
-  neu.bias = ((double)rand() / RAND_MAX) * 2 - 1;  // Random bias between -1 and 1
-  neu.z = 0.0f;
-  neu.d_actv = 0.0f;
-  neu.d_bias = 0.0f;
-  neu.d_z = 0.0f;
+    neuron neu;
+    neu.actv = 0.0f;
+    neu.bias = ((double)rand() / RAND_MAX) * 2 - 1;
+    neu.z = 0.0f;
+    neu.d_actv = 0.0f;
+    neu.d_bias = 0.0f;
+    neu.d_z = 0.0f;
 
-  if (out_connections > 0) {
-    neu.out_weights = calloc(out_connections, sizeof(double));
-    neu.d_weights = calloc(out_connections, sizeof(double));
+    if (out_connections > 0) {
+        neu.out_weights = calloc(out_connections, sizeof(double));
+        neu.d_weights = calloc(out_connections, sizeof(double));
 
-    for (int i = 0; i < out_connections; i++) {
-      neu.out_weights[i] = ((float)rand() / RAND_MAX) * 2 - 1;  // Random weight between -1 and 1
-      neu.d_weights[i] = 0.0f;
+        for (int i = 0; i < out_connections; i++) {
+            neu.out_weights[i] = ((double)rand() 
+        / RAND_MAX) * 2 - 1;
+            neu.d_weights[i] = 0.0f;
+        }
+    } else {
+        neu.out_weights = NULL;
+        neu.d_weights = NULL;
     }
-  }
 
-  else {
-    neu.out_weights = NULL;
-    neu.d_weights = NULL;
-  }
-
-  return neu;
+    return neu;
 }
-
 
 
 int create_network() {
 
-  layer * layer = malloc(sizeof(layer) * LAYERS);
+  LAYERS = malloc(sizeof(layer) * LAYER_NUMBER);
 
-  for (int i = 0; i < LAYERS; i++) {
+  for (int i = 0; i < LAYER_NUMBER; i++) {
 
-    layer[i] = create_layer(NEURONS_NUMBER[i]);
-
-    printf("Created Layer: %d\n", i+1);
-    printf("Number of Neurons in Layer %d: %d\n", i+1,layer[i].neuron_number);
+    LAYERS[i] = create_layer(NEURONS_NUMBER[i]);
 
     for (int j = 0; j < NEURONS_NUMBER[i]; j++) {
-      if (i < LAYERS - 1) {
+      if (i < LAYER_NUMBER - 1) {
         neuron neuron = create_neuron(NEURONS_NUMBER[i+1]);
-        layer[i].neurons[j] = neuron;
-
-        printf("Neuron %d in Layer %d created\n",j+1,i+1);
-
+        LAYERS[i].neurons[j] = neuron;
       }
       else {
         neuron neuron = create_neuron(0);
-        layer[i].neurons[j] = neuron;
-        printf("Neuron %d in Layer %d created\n",j+1,i+1);
+        LAYERS[i].neurons[j] = neuron;
       }
     }
   }
 }
 
 
+void forward_prop()
+{
 
+  for(int i = 1; i < LAYER_NUMBER; i++)
+  {
+    for(int j = 0; j < NEURONS_NUMBER[i]; j++)
+    {
+      LAYERS[i].neurons[j].z = LAYERS[i].neurons[j].bias;
 
+      for(int k = 0; k < NEURONS_NUMBER[i-1]; k++)
+      {
+        LAYERS[i].neurons[j].z  = LAYERS[i].neurons[j].z 
+          + ((LAYERS[i-1].neurons[k].out_weights[j])
+          * (LAYERS[i-1].neurons[k].actv));
+      }
 
+      // Relu Activation Function for Hidden LAYERSers
+      if (i < LAYER_NUMBER-1)
+      {
+        if ((LAYERS[i].neurons[j].z) < 0)
+        {
+          LAYERS[i].neurons[j].actv = 0;
+        }
 
+        else
+      {
+          LAYERS[i].neurons[j].actv = LAYERS[i].neurons[j].z;
+        }
+      }
 
-
-
-
-double** array_to_matrix(double* m1, size_t width_m1) {
-  double** res = malloc(width_m1 * sizeof(double*));
-  for (int i = 0; i < width_m1; i++) {
-    res[i] = calloc(1, sizeof(double));
-    res[i][0] = m1[i];
-  }
-  return res;
-}
-
-double** matrix_product(double** m1, size_t width_m1, size_t height_m1,
-                        double** m2, size_t width_m2, size_t height_m2) {
-
-  if (width_m1 != height_m2)
-    errx(EXIT_FAILURE, "In matrix_product, the matrix size are incorrect");
-
-  double** res = calloc(height_m1, sizeof(double*));
-  for (size_t i = 0; i < height_m1; i++) {
-    res[i] = calloc(width_m2, sizeof(double));
-  }
-
-  for (size_t i = 0; i < height_m1; i++) {
-    for (size_t j = 0; j < width_m2; j++) {
-      for (size_t k = 0; k < width_m1; k++) {
-        res[i][j] += m1[i][k] * m2[k][j];
+      // Sigmoid Activation function for Output LAYERSer
+      else
+    {
+        LAYERS[i].neurons[j].actv = 1/(1+exp(-LAYERS[i].neurons[j].z));
       }
     }
   }
-  return res;
 }
 
-double** matrix_sum(double** m1, size_t width_m1, size_t height_m1,
-                    double** m2, size_t width_m2, size_t height_m2) {
-  if (width_m1 != width_m2 || height_m1 != height_m2)
-    errx(EXIT_FAILURE, "In matrix_sum, the matrix size are incorrect");
 
-  double** res = malloc(height_m1 * sizeof(double));
+void back_prop(int p)
+{
+  int i, j, k;
 
-  for (size_t i = 0 ; i < height_m1; i++) {
-    res[i] = calloc(width_m1, sizeof(double));
-    for (size_t j = 0; j < width_m1; j++) {
-      res[i][j] = m1[i][j] + m2[i][j];
+  if (p < 0 || p >= 4 || DESIRED_OUTPUTS == NULL) {
+    fprintf(stderr, "Erreur: p (%d) est hors limites\n", p);
+    return;
+  }
+
+  for (j = 0; j < NEURONS_NUMBER[LAYER_NUMBER - 1]; j++)
+  {
+    if (LAYERS[LAYER_NUMBER - 1].neurons == NULL 
+      || DESIRED_OUTPUTS[p] == NULL) {
+      fprintf(stderr, 
+              "Erreur.\n");
+      return;
+    }
+
+    double actv = LAYERS[LAYER_NUMBER - 1].neurons[j].actv;
+    LAYERS[LAYER_NUMBER - 1].neurons[j].d_z = 
+      (actv - DESIRED_OUTPUTS[p][j]) * actv * (1 - actv);
+
+    for (k = 0; k < NEURONS_NUMBER[LAYER_NUMBER - 2]; k++)
+    {
+      LAYERS[LAYER_NUMBER - 2].neurons[k].d_weights[j] =
+        LAYERS[LAYER_NUMBER - 1].neurons[j].d_z 
+        * LAYERS[LAYER_NUMBER - 2].neurons[k].actv;
+
+      LAYERS[LAYER_NUMBER - 2].neurons[k].d_actv =
+        LAYERS[LAYER_NUMBER - 2].neurons[k].out_weights[j] 
+        * LAYERS[LAYER_NUMBER - 1].neurons[j].d_z;
+    }
+    LAYERS[LAYER_NUMBER - 1].neurons[j].d_bias = 
+      LAYERS[LAYER_NUMBER - 1].neurons[j].d_z;
+  }
+
+  for (i = LAYER_NUMBER - 2; i > 0; i--)
+  {
+    for (j = 0; j < NEURONS_NUMBER[i]; j++)
+    {
+      if (LAYERS[i].neurons[j].z >= 0) {
+        LAYERS[i].neurons[j].d_z = LAYERS[i].neurons[j].d_actv;
+      } else {
+        LAYERS[i].neurons[j].d_z = 0;
+      }
+
+      for (k = 0; k < NEURONS_NUMBER[i - 1]; k++)
+      {
+        LAYERS[i - 1].neurons[k].d_weights[j] =
+          LAYERS[i].neurons[j].d_z * LAYERS[i - 1].neurons[k].actv;
+
+        if (i > 1) {
+          LAYERS[i - 1].neurons[k].d_actv =
+            LAYERS[i - 1].neurons[k].out_weights[j] 
+            * LAYERS[i].neurons[j].d_z;
+        }
+      }
+      LAYERS[i].neurons[j].d_bias = LAYERS[i].neurons[j].d_z;
+    }
+  }
+}
+
+void save_weights(const char *filename) {
+  FILE *file = fopen(filename, "w");
+  if (file == NULL) {
+    perror("Failed to open file for writing");
+    return;
+  }
+
+  for (int i = 0; i < LAYER_NUMBER; i++) {
+    for (int j = 0; j < NEURONS_NUMBER[i]; j++) {
+      fprintf(file, "%d,%d,%f", i, j, LAYERS[i].neurons[j].bias);
+      for (int k = 0; k < NEURONS_NUMBER[i + 1]; k++) {
+        fprintf(file, ",%f", LAYERS[i].neurons[j].out_weights[k]);
+      }
+      fprintf(file, "\n");
     }
   }
 
-  return res;
+  fclose(file);
 }
 
 
-double sigmoid (double x) {
-  return (1 / (1 + exp(-x)));
-}
-
-
-double sigmoid_derviate (double x) {
-  return sigmoid(x) * (1 - sigmoid(x));
-}
-
-double sigmoid_derivative(double x) {
-  return (x * (1 - x));
-}
-
-void initialize_weights(double weights[], int size) {
-  double random;
-  for (int i = 0; i < size; i++) {
-    random = (double)rand() / (double)RAND_MAX * 2.0f - 1.0f;
-    weights[i] = random;
-  }
-}
-
-void initialize_biases(double biases[], int size) {
-  double random;
-  for (int i = 0; i < size; i++) {
-    random = (double)rand() / (double)RAND_MAX * 2.0f - 1.0f;
-    biases[i] = random;
-  }
-}
-
-void printMatrix(double** matrix, int rows, int cols) {
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {
-      printf("%lf ", matrix[i][j]);
-    }
-    printf("\n");
-  }
-}
-
-double forward_propagation(double* input, double** weights_hidden, double* biases_hidden, double* weights_output, double bias_output, double* hidden_output) {
-
-  size_t weights_hidden_height = INPUT_NEURONS;
-  size_t weights_hidden_width = HIDDEN_NEURONS;
-
-  size_t weights_output_width = HIDDEN_NEURONS;
-
-  size_t input_width = INPUT_NEURONS;
-
-  size_t biases_hidden_width = HIDDEN_NEURONS;
-
-  double** input_in_matrix = array_to_matrix(input, input_width);
-
-  size_t hidden_output_width = HIDDEN_NEURONS;
-
-  double** hidden_output_matrix = calloc(1, sizeof(double*));
-  hidden_output_matrix[0] = calloc(hidden_output_width, sizeof(double));
-
-
-  hidden_output_matrix = matrix_product(weights_hidden, weights_hidden_width, weights_hidden_height,
-                                        input_in_matrix, 1, input_width);
-
-  hidden_output_matrix = matrix_sum(hidden_output_matrix, 1, hidden_output_width,
-                                    array_to_matrix(biases_hidden, biases_hidden_width), 1, biases_hidden_width);
-
-  for (size_t i = 0; i < hidden_output_width; i++) {
-    hidden_output[i] = sigmoid(hidden_output_matrix[i][0]);
-  }
-
-
-  double** final_output_matrix = matrix_product(array_to_matrix(weights_output, weights_output_width), weights_output_width, 1,
-                                                array_to_matrix(hidden_output, hidden_output_width), 1, hidden_output_width);
-
-  double final_output = final_output_matrix[0][0] + bias_output;
-  final_output = sigmoid(final_output);
-
-  return final_output;
-}
-
-void backward_propagation(double* input, double* hidden_output, double output,
-                          double expected_output, double* weights_hidden[], double* biases_hidden,
-                          double* weights_output, double* bias_output) {
-
-  double output_error = output - expected_output;
-  double output_delta = output_error * output * (1 - output);
-
-  for (size_t i = 0; i < HIDDEN_NEURONS; i++) {
-    weights_output[i] -= LEARNING_RATE * output_delta * hidden_output[i];
-  }
-  *bias_output -= LEARNING_RATE * output_delta;
-
-  double hidden_delta[HIDDEN_NEURONS];
-  for (size_t i = 0; i < HIDDEN_NEURONS; i++) {
-    double hidden_error = output_delta * weights_output[i];
-    hidden_delta[i] = hidden_error * hidden_output[i] * (1 - hidden_output[i]); 
-  }
-
-  for (size_t i = 0; i < INPUT_NEURONS; i++) {
-    for (size_t j = 0; j < HIDDEN_NEURONS; j++) {
-      weights_hidden[i][j] -= LEARNING_RATE * hidden_delta[j] * input[i];
-    }
-  }
-
-  for (size_t i = 0; i < HIDDEN_NEURONS; i++) {
-    biases_hidden[i] -= LEARNING_RATE * hidden_delta[i];
-  }
-}
-
-
-// cross entropy loss
-double calculate_error(double* targets, double* predictions, size_t size) { 
-  double epsilon = 1e-15;
-  double loss = 0.0;
-
-  for (size_t i = 0; i < size; i++) {
-    double pred = predictions[i];
-    if (pred < epsilon) {
-      pred = epsilon;
-    }
-    if (pred > 1 - epsilon) {
-      pred = 1 - epsilon;
+void load_weights(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Failed to open file for reading");
+        return;
     }
 
-    loss += targets[i] * log(pred) + (1 - targets[i]) * log(1 - pred);
-  }
+    int layer_index, neuron_index;
+    double bias;
 
-  loss = -loss / size;
+    while (fscanf(file, "%d,%d,%lf", &layer_index, 
+                  &neuron_index, &bias) == 3) {
+        LAYERS[layer_index].neurons[neuron_index].bias = bias;
 
-  return loss;
+        for (int k = 0; k < NEURONS_NUMBER[layer_index + 1]; k++) {
+            double weight;
+            if (fscanf(file, ",%lf", &weight) != 1) {
+                fprintf(stderr, 
+                        "Erreur de lecture des poids\n");
+                fclose(file);
+                return;
+            }
+            LAYERS[layer_index].neurons[neuron_index].out_weights[k] = weight;
+        }
+    }
+
+    fclose(file);
+    printf("Weights loaded from %s\n", filename);
 }
 
 
-int main() {
+int main(int argc, char** argv) {
+
+  int train = 0;
+  if (argc > 1) {
+    if (argc == 2) {
+      if (strcmp(argv[1], "TRAIN") == 0)
+        train = 1;
+      else {
+        err(EXIT_FAILURE, "Parameter must be : './XOR TRAIN' or './XOR'");
+      }
+    }
+    else {
+        err(EXIT_FAILURE, "Parameter must be : './XOR TRAIN' or './XOR'");
+    }
+  }
+  srand(time(0)); 
   create_network();
+
+
+  int input_data[4][2] = {
+    {0, 0},
+    {0, 1},
+    {1, 0},
+    {1, 1}
+  };
+  int output_data[4][1] = {
+    {0},
+    {1},
+    {1},
+    {0}
+  };
+
+  DESIRED_OUTPUTS = malloc(sizeof(int*) * 4);
+  for (int i = 0; i < 4; i++) {
+    DESIRED_OUTPUTS[i] = malloc(sizeof(int) 
+                                * NEURONS_NUMBER[LAYER_NUMBER - 1]);
+    for (int j = 0; j < NEURONS_NUMBER[LAYER_NUMBER - 1]; j++) {
+      DESIRED_OUTPUTS[i][j] = output_data[i][j];
+    }
+  }
+
+  if (train) {
+    for (int epoch = 0; epoch < EPOCHS; epoch++) {
+      double total_error = 0;
+
+      for (int p = 0; p < 4; p++) {
+        LAYERS[0].neurons[0].actv = input_data[p][0];
+        LAYERS[0].neurons[1].actv = input_data[p][1];
+
+        forward_prop();
+
+        double output = LAYERS[LAYER_NUMBER - 1].neurons[0].actv;
+        double error = output - DESIRED_OUTPUTS[p][0];
+        total_error += error * error;
+
+        printf("Error probability : %f \n\n", total_error);
+
+        back_prop(p);
+
+        for (int i = 1; i < LAYER_NUMBER; i++) {
+          for (int j = 0; j < NEURONS_NUMBER[i]; j++) {
+            LAYERS[i].neurons[j].bias -= LEARNING_RATE 
+              * LAYERS[i].neurons[j].d_bias;
+            for (int k = 0; k < NEURONS_NUMBER[i - 1]; k++) {
+              LAYERS[i - 1].neurons[k].out_weights[j] -= 
+                LEARNING_RATE * LAYERS[i - 1].neurons[k].d_weights[j];
+            }
+          }
+        }
+      }
+      save_weights("weights.txt");
+      if (epoch % (EPOCHS / 10) == 0) {
+        printf("Epoch %d, Mean Squared Error: %f\n", epoch, total_error / 4);
+      }
+    }
+  }
+  else {
+    load_weights("weights.txt");
+  }
+  
+  if (train) {
+    return EXIT_SUCCESS;
+  }
+  else 
+    printf("\nTest du réseau sans entraînement:\n\n");
+  for (int p = 0; p < 4; p++) {
+    LAYERS[0].neurons[0].actv = input_data[p][0];
+    LAYERS[0].neurons[1].actv = input_data[p][1];
+
+    forward_prop();
+
+    double output = LAYERS[LAYER_NUMBER - 1].neurons[0].actv;
+    printf(
+      "Entrée: [%d, %d] => Sortie prédite: %.2f, Sortie attendue: %d\n\n",
+           input_data[p][0], input_data[p][1], output, output_data[p][0]);
+  }
+
+  for (int i = 0; i < 4; i++) {
+    free(DESIRED_OUTPUTS[i]);
+  }
+  free(DESIRED_OUTPUTS);
+  for (int i = 0; i < LAYER_NUMBER; i++) {
+    free(LAYERS[i].neurons);
+  }
+  free(LAYERS);
+
   return 0;
 }
 
