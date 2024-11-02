e0ef1b5fa9995a57a14a4f40c77759fc682487f2
Arnaud <arnaud.puit@epita.fr>
2024-11-02 15:53:32 +0100

    Fixe bugs

--- LineDetection/LineDetection.c
+++ LineDetection/LineDetection.c
@@ -34,19 +34,14 @@ int** detectEdges(SDL_Surface* surface) {
     int** res = malloc((height - 2) * sizeof(int*));
     double** matrix = getBrightness(surface);
 
-    Uint32* pixels = (Uint32*)surface->pixels;
-
     if (matrix == NULL) return NULL;
 
     for (int y = 1; y < height - 1; y++) {
         res[y-1] = malloc((width - 2) * sizeof(int));
 
         for (int x = 1; x < width - 1; x++) {
-            int temp = (int)sqrt(pow(-1 * matrix[y-1][x-1] - 2 * matrix[y][x-1] - 1 * matrix[y+1][x-1] + matrix[y-1][x+1] + 2 * matrix[y][x+1] + matrix[y+1][x+1], 2)
+            res[y-1][x-1] = (int)sqrt(pow(-1 * matrix[y-1][x-1] - 2 * matrix[y][x-1] - 1 * matrix[y+1][x-1] + matrix[y-1][x+1] + 2 * matrix[y][x+1] + matrix[y+1][x+1], 2)
                              + pow(-1 * matrix[y-1][x-1] - 2 * matrix[y-1][x] - 1 * matrix[y-1][x+1] + matrix[y+1][x-1] + 2 * matrix[y+1][x] + matrix[y+1][x+1], 2)) / 4;
-            
-            pixels[y * width + x] = SDL_MapRGB(surface->format, temp, temp, temp);
-            res[y-1][x-1] = temp;
         }
     }
 
@@ -75,15 +70,8 @@ void detectLines(char* filepath) {
         return;
     }
 
-    if (SDL_SaveBMP(surface, filepath) != 0) {
-        printf("Image saving error: %s\n", SDL_GetError());
-    }
-    else {
-        printf("Image processed successfully and saved as '%s'.\n", filepath);
-    }
 
-
-    int threshold,
+    int threshold = 0,
         width = surface->w - 2,
         height = surface->h - 2,
         size = (int)sqrt(pow((double)height, 2) + pow((double)width, 2)) + 1;
@@ -106,113 +94,112 @@ void detectLines(char* filepath) {
             for (int theta = 0; theta < 180; theta++) {
 
                 rho = x * cos(theta * M_PI / 180) + y * sin(theta * M_PI / 180);
-                if (++accumulatorArray[rho + size][theta] > threshold) {
+                accumulatorArray[rho + size][theta]++;
+                if (accumulatorArray[rho + size][theta] > threshold) {
                     threshold = accumulatorArray[rho + size][theta];
                 }
             }
         }
     }
 
-    threshold -= threshold / 4;
-
-    char* title = "Lines";
-    Uint32 flags = SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE;
-    SDL_Window* window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    threshold -= threshold / 5;
 
     for (int rho = 0; rho < size * 2; rho++) {
         for (int theta = 0; theta < 180; theta++) {
-
+            
             if (accumulatorArray[rho][theta] > threshold) {
-                drawLine(renderer, (double)rho - size, (double)(theta * M_PI / 180), height, width);
+                drawLine(surface, (double)(rho - size), (double)theta, height, width);
             }
         }
         free(accumulatorArray[rho]);
     }
     free(accumulatorArray);
 
-	SDL_RenderPresent(renderer);
 
-
-    int keep_alive = 1;
-	while (keep_alive)
-	{
-		keep_alive = process_events();
-	}
-	close_program(window, renderer);
+    if (SDL_SaveBMP(surface, filepath) != 0) {
+        printf("Image saving error: %s\n", SDL_GetError());
+    }
+    else {
+        printf("Image processed successfully and saved as '%s'.\n", filepath);
+    }
 }
 
 
-void drawLine(SDL_Renderer* renderer, double rho, double theta, int h, int w) {
 
-    // Calculate the endpoints of the line
-    double cos_theta = cos(theta);
-    double sin_theta = sin(theta);
+void drawLine(SDL_Surface* surface, double rho, double theta, int h, int w) {
 
-    // Calculate the intersection points with the borders of the image
     double x1, y1, x2, y2;
 
-    // Handle vertical lines
-    if (fabs(cos_theta) < 1e-6) {
-        // Vertical line: use rho to set x-coordinates
-        x1 = x2 = rho; // rho is the x position
-        y1 = 0; // Top edge
-        y2 = h; // Bottom edge
+    if (theta == 90) {
+        x1 = 0;
+        x2 = w;
+        y1 = y2 = rho;
     }
-    // Handle horizontal lines
-    else if (fabs(sin_theta) < 1e-6) {
-        // Horizontal line: use rho to set y-coordinates
-        y1 = y2 = rho; // rho is the y position
-        x1 = 0; // Left edge
-        x2 = w; // Right edge
-    } else {
-        // Calculate intersections with the image borders
-        // Left border (x = 0)
+    else if (theta == 0) {
+        x1 = x2 = rho;
+        y1 = 0;
+        y2 = h;
+    }
+    else {
+
+        theta *= M_PI / 180;
+        double cos_theta = cos(theta), sin_theta = sin(theta);
+
         y1 = rho / sin_theta;
-        if (y1 < 0) { // If it doesn't intersect, use the top border
+        if (y1 < 0) {
             y1 = 0;
             x1 = rho / cos_theta;
-        } else if (y1 > h) { // If it doesn't intersect, use the bottom border
+        }
+        else if (y1 > h) {
             y1 = h;
             x1 = (rho - h * sin_theta) / cos_theta;
-        } else { // Valid intersection with the left border
+        }
+        else {
             x1 = 0;
         }
 
-        // Right border (x = width)
         y2 = (rho - w * cos_theta) / sin_theta;
-        if (y2 < 0) { // If it doesn't intersect, use the top border
+        if (y2 < 0) {
             y2 = 0;
             x2 = rho / cos_theta;
-        } else if (y2 > h) { // If it doesn't intersect, use the bottom border
+        }
+        else if (y2 > h) {
             y2 = h;
             x2 = (rho - h * sin_theta) / cos_theta;
-        } else { // Valid intersection with the right border
+        }
+        else {
             x2 = w;
         }
     }
 
-    // Set the draw color (optional)
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red color for the line
-
-    // Draw the line using SDL
-    SDL_RenderDrawLine(renderer, (int)x1, (int)y1, (int)x2, (int)y2);
+    drawLineOnSurface(surface, x1, y1, x2, y2);
 }
 
-void close_program(SDL_Window *window, SDL_Renderer *renderer) {
+void drawLineOnSurface(SDL_Surface *surface, int x1, int y1, int x2, int y2) {
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+    int err = dx - dy;
 
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
-
-	SDL_QuitSubSystem(SDL_INIT_VIDEO);
-	SDL_Quit();
+    while (x1 != x2 || y1 != y2) {
+        put_pixel(surface, x1, y1);
+        
+        int e2 = err * 2;
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
 }
 
-int process_events() {
-
-	SDL_Event event;
-	while (SDL_PollEvent(&event)) {
-		if (event.type == SDL_QUIT) return 0;
-	}
-	return 1;
+void put_pixel(SDL_Surface *surface, int x, int y) {
+    if (x >= 0 && x < surface->w && y >= 0 && y < surface->h) {
+        Uint32* pixels = (Uint32*)surface->pixels;
+        pixels[y * surface->w + x] = SDL_MapRGB(surface->format, 255, 0, 0);
+    }
 }
--- LineDetection/LineDetection.h
+++ LineDetection/LineDetection.h
@@ -6,8 +6,8 @@
 double** getBrightness(SDL_Surface* surface);
 int** detectEdges(SDL_Surface* surface);
 void detectLines(char* filepath);
-void drawLine(SDL_Renderer* renderer, double rho, double theta, int h, int w);
-void close_program(SDL_Window *window, SDL_Renderer *renderer);
-int process_events();
+void drawLine(SDL_Surface* surface, double rho, double theta, int h, int w);
+void drawLineOnSurface(SDL_Surface *surface, int x1, int y1, int x2, int y2);
+void put_pixel(SDL_Surface *surface, int x, int y);
 
 #endif
