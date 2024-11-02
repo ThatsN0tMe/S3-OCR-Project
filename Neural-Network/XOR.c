0ab1ac27a73c36972dd26ed47b7ed7f86f1a0c25
ad <adrien.malgouyres@epita.fr>
2024-11-02 14:56:22 +0100

    [ADD] Parametre can have learing_rate and epochs

--- Neural-Network/XOR.c
+++ Neural-Network/XOR.c
@@ -6,8 +6,6 @@
 #include <string.h>
 
 #define LAYER_NUMBER 3
-#define LEARNING_RATE 0.001
-#define EPOCHS 100000
 
 typedef struct neuron_t
 {
@@ -35,6 +33,8 @@ typedef struct layer_t
 layer* LAYERS;
 int NEURONS_NUMBER[LAYER_NUMBER] = {2, 2, 1};
 int** DESIRED_OUTPUTS;
+double LEARNING_RATE = 0.001;
+int EPOCHS = 100000;
 
 
 
@@ -47,29 +47,29 @@ layer create_layer(int neuron_number) {
 }
 
 neuron create_neuron(int out_connections) {
-    neuron neu;
-    neu.actv = 0.0f;
-    neu.bias = ((double)rand() / RAND_MAX) * 2 - 1;
-    neu.z = 0.0f;
-    neu.d_actv = 0.0f;
-    neu.d_bias = 0.0f;
-    neu.d_z = 0.0f;
+  neuron neu;
+  neu.actv = 0.0f;
+  neu.bias = ((double)rand() / RAND_MAX) * 2 - 1;
+  neu.z = 0.0f;
+  neu.d_actv = 0.0f;
+  neu.d_bias = 0.0f;
+  neu.d_z = 0.0f;
 
-    if (out_connections > 0) {
-        neu.out_weights = calloc(out_connections, sizeof(double));
-        neu.d_weights = calloc(out_connections, sizeof(double));
+  if (out_connections > 0) {
+    neu.out_weights = calloc(out_connections, sizeof(double));
+    neu.d_weights = calloc(out_connections, sizeof(double));
 
-        for (int i = 0; i < out_connections; i++) {
-            neu.out_weights[i] = ((double)rand() 
+    for (int i = 0; i < out_connections; i++) {
+      neu.out_weights[i] = ((double)rand() 
         / RAND_MAX) * 2 - 1;
-            neu.d_weights[i] = 0.0f;
-        }
-    } else {
-        neu.out_weights = NULL;
-        neu.d_weights = NULL;
+      neu.d_weights[i] = 0.0f;
     }
+  } else {
+    neu.out_weights = NULL;
+    neu.d_weights = NULL;
+  }
 
-    return neu;
+  return neu;
 }
 
 
@@ -220,48 +220,56 @@ void save_weights(const char *filename) {
 
 
 void load_weights(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Failed to open file for reading");
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    perror("Failed to open file for reading");
+    return;
+  }
+
+  int layer_index, neuron_index;
+  double bias;
+
+  while (fscanf(file, "%d,%d,%lf", &layer_index, 
+                &neuron_index, &bias) == 3) {
+    LAYERS[layer_index].neurons[neuron_index].bias = bias;
+
+    for (int k = 0; k < NEURONS_NUMBER[layer_index + 1]; k++) {
+      double weight;
+      if (fscanf(file, ",%lf", &weight) != 1) {
+        fprintf(stderr, 
+                "Erreur de lecture des poids\n");
+        fclose(file);
         return;
+      }
+      LAYERS[layer_index].neurons[neuron_index].out_weights[k] = weight;
     }
+  }
 
-    int layer_index, neuron_index;
-    double bias;
-
-    while (fscanf(file, "%d,%d,%lf", &layer_index, 
-                  &neuron_index, &bias) == 3) {
-        LAYERS[layer_index].neurons[neuron_index].bias = bias;
-
-        for (int k = 0; k < NEURONS_NUMBER[layer_index + 1]; k++) {
-            double weight;
-            if (fscanf(file, ",%lf", &weight) != 1) {
-                fprintf(stderr, 
-                        "Erreur de lecture des poids\n");
-                fclose(file);
-                return;
-            }
-            LAYERS[layer_index].neurons[neuron_index].out_weights[k] = weight;
-        }
-    }
-
-    fclose(file);
-    printf("Poids et biais chargés depuis %s\n", filename);
+  fclose(file);
+  printf("Poids et biais chargés depuis %s\n", filename);
 }
 
 
 int main(int argc, char** argv) {
-
   int train = 0;
   if (argc > 1) {
-    if (argc == 2) {
-      if (strcmp(argv[1], "TRAIN") == 0)
+    if (argc >= 2) {
+      if (strcmp(argv[1], "TRAIN") == 0) {
         train = 1;
-      else {
-        err(EXIT_FAILURE, "Parameter must be : './XOR TRAIN' or './XOR'");
       }
-    }
-    else {
+      if (argc == 4) {
+        double learning_r = atof(argv[2]);
+        int epoc = atoi(argv[3]);
+        if (learning_r <= 0 || epoc <= 0) {
+          err(EXIT_FAILURE,
+              "LEARNING_RATE and EPOCHS must be >= 0");
+        }
+        else {
+          LEARNING_RATE = learning_r;
+          EPOCHS = epoc;
+        }
+      }
+      if (argc > 4)
         err(EXIT_FAILURE, "Parameter must be : './XOR TRAIN' or './XOR'");
     }
   }
@@ -327,19 +335,18 @@ int main(int argc, char** argv) {
       }
       save_weights("weights.txt");
       if (epoch % (EPOCHS / 10) == 0) {
-        printf("Epoch %d, Mean Squared Error: %f\n", epoch, total_error / 4);
       }
     }
   }
   else {
     load_weights("weights.txt");
   }
-  
+
   if (train) {
     return EXIT_SUCCESS;
   }
   else 
-    printf("\nTest du réseau sans entraînement:\n\n");
+  printf("\nTest du réseau sans entraînement:\n\n");
   for (int p = 0; p < 4; p++) {
     LAYERS[0].neurons[0].actv = input_data[p][0];
     LAYERS[0].neurons[1].actv = input_data[p][1];
@@ -349,7 +356,7 @@ int main(int argc, char** argv) {
     double output = LAYERS[LAYER_NUMBER - 1].neurons[0].actv;
     printf(
       "Entrée: [%d, %d] => Sortie prédite: %.2f, Sortie attendue: %d\n\n",
-           input_data[p][0], input_data[p][1], output, output_data[p][0]);
+      input_data[p][0], input_data[p][1], output, output_data[p][0]);
   }
 
   for (int i = 0; i < 4; i++) {
