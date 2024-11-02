aa6abb22a5496817056e162248a5e9800c35fa67
Corentin <corentin.louis74@gmail.com>
2024-11-02 15:39:46 +0100

    aaaaaa

--- Pretreatment/pretreatment.h
+++ Pretreatment/pretreatment.h
@@ -8,7 +8,7 @@ void contrast(SDL_Surface *surface, float contrastFactor);
 void binarize(SDL_Surface *surface, Uint8 threshold);
 void gaussian(SDL_Surface *surface, int kernel_size);
 void median(SDL_Surface *surface, int kernel_size);
-Uint32 get_major_color(SDL_Surface *surface, int white);
+Uint32 get_major_color(SDL_Surface *surface);
 double variance(SDL_Surface *surface);
 
 void ApplyPretreatment(char *filepath);
