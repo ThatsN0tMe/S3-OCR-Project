0634ac283a3e6b5d3954f9c7a2dc555aa758f881
Arnaud <arnaud.puit@epita.fr>
2024-12-06 13:47:44 +0100

    ntm viktor

--- Detection/WordDetection.c
+++ Detection/WordDetection.c
@@ -5,16 +5,31 @@
 #include "Detection.h"
 
 
+int getNumWords(SDL_Surface* surface) {
+    int numWords = 0,
+        nextLine = 1;
+    for (int y = 0; y < surface->h - 1; y += 7) {
+        if (isWhiteLine(surface, 0, y, surface->w - 1, y)) {
+            nextLine = 1;
+        }
+        else if (nextLine) {
+            numWords++;
+            nextLine = 0;
+        }
+    }
+
+    return numWords;
+}
+
+
 void detectWords(SDL_Surface* surface, char* filepath) {
 
     int width = surface->w,
-        height = surface->h;
-    
-    int numWords = 0,
+        height = surface->h,
         nextBottomLine = 0,
         previousLineY = 0;
 
-    for (int y = 0; y < width; y++) {
+    for (int y = 0; y < height; y++) {
         if (!isWhiteLine(surface, 0, y, width - 1, y)) {
             if (!nextBottomLine) {
                 drawLineOnSurface(surface, 0, previousLineY, width-1, previousLineY);
@@ -25,7 +40,6 @@ void detectWords(SDL_Surface* surface, char* filepath) {
             if (nextBottomLine) {
                 drawLineOnSurface(surface, 0, y, width-1, y);
                 nextBottomLine = 0;
-                numWords++;
             }
             previousLineY = y;
         }
